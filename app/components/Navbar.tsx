98f804325d5d5ecb8c0490cf46b708539a06884e
69-69 <devmail026@gmail.com>
2025-04-25 14:15:59 -0500

    + nav resolved

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -106,7 +106,7 @@ const Navbar: React.FC<{ userSession: UserSession | null, sideMenuLinks?: AppLin
                             justifyContent: 'flex-end'
                         }}>
                             {/* Web menu */}
-                            <WebMenu isSession={user !== null}/>
+                            <WebMenu isSession={userSession !== null}/>
 
                             {/* Cart, Mail, Notification, Account */}
                             <CartButton count={5}/>
