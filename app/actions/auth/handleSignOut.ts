530b37c4d4bc12609164fb6b5771dceb486dacaa
69-69 <devmail026@gmail.com>
2025-04-28 12:45:52 -0500

    + refactoring

--- app/actions/auth/handleSignOut.ts
+++ app/actions/auth/handleSignOut.ts
@@ -43,7 +43,7 @@ export const signOut = async (): Promise<void> => {
         const apiClient = await getApiClientWithAuth();
         await apiClient.request({method: 'POST', url: `/${signOutEndpoint}`});
 
-        await clearCookiesAndRedirect();
+        return await clearCookiesAndRedirect();
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
         // console.error("Error during sign-out:", error);
