c02c0f449b26acaa4fa1ba5f750efeb5716dee8d
69-69 <devmail026@gmail.com>
2025-04-28 11:25:53 -0500

    + Error Suspense boundary resolved

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,24 +1,28 @@
 'use client';
-import React from 'react';
+import React, {Suspense} from 'react';
 import Container from "@mui/material/Container";
 import SignInForm from "@/app/components/auth/SignInForm";
 import SessionStatusSnackbar from "@/app/components/SessionStatusSnackbar";
-import { useSearchParams } from 'next/navigation';
+import {useSearchParams} from 'next/navigation';
 
 const SignInPage = () => {
-    const searchParams = useSearchParams()
+    const searchParams = useSearchParams();
 
-    const isLoggingOut = searchParams.get('logout') === 'true';
+    const getLogout = searchParams.get('logout') || '';
+    const isLoggingOut = getLogout === 'true';
 
     return (
-        <Container maxWidth='sm' sx={{flexGrow: 1, pt: 20}}>
-            {/* Show snackbar only if the user is logging out */}
-            {isLoggingOut && <SessionStatusSnackbar
-                isOpen={isLoggingOut}
-                message="You've been logged out"
-            />}
-            <SignInForm/>
-        </Container>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Container maxWidth='sm' sx={{flexGrow: 1, pt: 20}}>
+                {/* Show snackbar only if the user is logging out */}
+                {
+                    isLoggingOut && <SessionStatusSnackbar
+                        isOpen={isLoggingOut}
+                        message="You've been logged out"/>
+                }
+                <SignInForm/>
+            </Container>
+        </Suspense>
     );
 };
 
